d512784ecde7dbc8cabb7e43b0e132063350d3c1
Nariman Yousefzadeh <126755476+NarimanYou@users.noreply.github.com>
2024-04-05 14:44:31 -0400

    Practicing with Git and GitHub

--- KirilSpiridonov_CoursePython/main.py
+++ KirilSpiridonov_CoursePython/main.py
@@ -1,3 +1,13 @@
 lst = [3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
 lst.pop()
+print(lst) 
+
+print(len(lst))
+print(sum(lst))
+print(max(lst))
+print(min(lst))
+print(lst.index(5))
+print(lst.count(5))
+lst.remove(3)
 print(lst)
+
