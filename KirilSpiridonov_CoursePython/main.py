99669cc4605111e050960f7f4c50250e0101c755
Nariman Yousefzadeh <126755476+NarimanYou@users.noreply.github.com>
2024-04-05 15:36:32 -0400

    Exploring Lists

--- KirilSpiridonov_CoursePython/main.py
+++ KirilSpiridonov_CoursePython/main.py
@@ -1,13 +1,40 @@
 lst = [3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
-lst.pop()
-print(lst) 
 
-print(len(lst))
-print(sum(lst))
-print(max(lst))
-print(min(lst))
-print(lst.index(5))
-print(lst.count(5))
-lst.remove(3)
-print(lst)
+#lst.pop()
+#print(lst) 
+#print(len(lst))
+#print(sum(lst))
+#print(max(lst))
+#print(min(lst))
+#print(lst.index(5))
+#print(lst.count(5))
+#lst.remove(3)
+#print(lst)
+#lst.clear()
+#print(lst)
+#print(sorted(lst)[: :-1])
+
+
+fruits = ["apple", "banana", "orange"]
+numbers = [1, 2, 3, 4, 5]
+mixed_list = [1, "apple", True, 3.14]
+
+# print(fruits)
+# print(numbers)
+# print(mixed_list)
+
+# print(fruits[0])     
+# print(fruits[1])    
+# print(fruits[2])
+
+#Slicing:
+
+# print(numbers[1:4])   
+# print(numbers[:3])    
+# print(numbers[2:]) 
+
+#Lists are mutable:  
+fruits[1] = "grape"
+print(fruits)
+
 
